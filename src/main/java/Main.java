b9219d6bbe331fe3f053a2988ac2d85eedc75b20
이승환 <seaung13@gmail.com>
2024-11-23 03:13:31 +0900

    MyArray 테스트 코드 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/test/java/array/MyArrayTest.java
+++ src/test/java/array/MyArrayTest.java
@@ -1,7 +1,43 @@
 package array;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MyArrayTest {
+
+    @DisplayName("MyArray는 size() 메서드로 크기를 확인할 수 있다.")
+    @Test
+    void size() {
+        // given
+        MyArray<String> array = new MyArray<>(5);
+
+        // when
+        int size = array.size();
+
+        // then
+        assertThat(size).isEqualTo(5);
+    }
+
+    @DisplayName("MyArray는 인덱스로 접근해 데이터를 변경할 수 있다.")
+    @Test
+    void set() {
+        // given
+        MyArray<String> array = new MyArray<>(5);
+
+        // when
+        array.set(0, "Hello");
+        array.set(1, "World");
+
+        // then
+        assertThat(array.get(0)).isEqualTo("Hello");
+        assertThat(array.get(1)).isEqualTo("World");
+
+    }
+
+
 
 }
