cc9d981286d78f3f46f48b292b7a381da20d05c8
이승환 <seaung13@gmail.com>
2024-11-26 01:37:15 +0900

    arrayList 디렉토리 생성

---
+++ src/main/java/arrayList/MyArrayList.java
@@ -0,0 +1,9 @@
+package arrayList;
+
+public class MyArrayList {
+
+    // Array는 만들때 크기를 정해야하고 이후 변경이 불가능하죠
+    // 이를 보완해서 요소를 넣는대로 크기가 변경되도록 보완한 게 ArrayList입니다.
+    // Array는 정적 배열, ArrayList는 동적 배열이라고 기억하시면 됩니다.
+    //
+}
